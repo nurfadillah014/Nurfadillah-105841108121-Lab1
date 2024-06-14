2c4f1c47737b5e02619e4509b4dac37a00d4639c
nurfadillah014 <105841108121@student.unismuh.ac.id>
2024-06-14 16:16:19 +0800

    text Input

--- App.js
+++ App.js
@@ -1,149 +1,35 @@
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
-//  const App = () => {
-//    return(
-//      <View style = {{
-//        flex:1,
-//      }}>
-//        <View style = {{
-//          flex: 1,flexDirection : 'row',
-//          backgroundColor : 'white'
-//        }}>
-//        <View style = {{
-//          flex:1,
-//          backgroundColor : 'yellow'
-//        }}></View>
-//        <View style = {{
-//          flex:1,
-//          backgroundColor : 'green'
-//        }}></View>
-//        </View>
-//        <View style = {{
-//          flex:1,
-//          backgroundColor : 'red'
-//        }}></View>
-//        <View style = {{
-//          flex: 1,
-//          backgroundColor : 'blue'
-//        }}></View>
-//        </View>
-  
-//    )
-// }
+import { StyleSheet, Text, View, TextInput } from "react-native"
+import React from "react"
 
-// export default App
-
-
-
-// import { StyleSheet,Text, View, Image } from 'react-native'
-// import React from 'react';
-
-// const App = () => {
-//   return (
-//     <View style={{flex : 1}}>
-//       <View style={{
-
-//         flex: 1,
-//         alignItems: 'flex-end', //BARIS
-//         justifyContent: 'center', //KOLOM
-//         flexDirection: 'row',
-//         marginBottom: 100,
-//         justifyContent: 'space-evenly'
-
-
-
-//       }}>
-//         <View style ={{
-//           backgroundColor: 'red', width: 180, height: 80,
-//           justifyContent: 'center',
-//           borderRadius: 10,
-
-
-//         }}>
-
-//           <Text style= {{
-//             fontSize: 30, color: 'white',
-//             textAlign: 'center',
-//             fontWeight: 'bold',
-//           }}>
-
-//             Login
-//              </Text>
-//        </View>
-
-//        <View style ={{
-//         backgroundColor: 'blue', width: 180, height: 80,
-//         justifyContent: 'center',
-//         borderRadius: 10,
-
-//        }}>
-
-//         <Text style= {{
-//           fontSize: 30, color: 'white',
-//           textAlign: 'center',
-//           fontWeight: 'bold',
-//         }}>
-
-//           Register 
-//            </Text>
-//      </View>
-//     </View>
-//     </View>
-//   );
-// }
-// export default App
-
-
-
-
-import { StyleSheet, Text, View, Image} from 'react-native'
-import React from 'react'
-import ButtonCostum from './component/button/button'
-
-const App = () => {
-  
-  return(
-    <View style={{flex : 1}}>
-      <View style={{
-        flex : 1,
-        alignItems : 'center',
-        justifyContent : 'center',
-
-      }}>
-        <Text style= {{
-          fontSize : 50,
-          fontWeight: 'bold'
-
-        }}>green bird</Text>
-    </View>
-
-
-    <View style={{
+const App =() => {
+  const TextInputCostum = ({ name, color }) =>{
+    return (
+     <TextInput
+     placeholder={`Masukkan ${name}`}
+     style={{
+      height : 60, 
+      width : 300,
+      borderColor : 'gray',
+      borderWidth : 1,
+      color : color,
+      padding : 10,
+      margin : 10,
+      borderRadius : 10,
+      paddingLeft : 10,
+      }}
+    />
+  )
+}
+  return (
+  <View style = {{
     flex : 1,
-    alignItems : 'center',
     justifyContent : 'center',
-
-    }}>ww
-    <Image source={require('./TokoPedia.png')} />
-    </View>
-
-    <View style={{
-    flex : 1,
     alignItems : 'center',
-    justifyContent : 'center',
-    flexDirection : 'row',
-    justifyContent : 'space-evenly'
-
-    }}>
-<ButtonCostum color = 'yellow' text='login' />
-<ButtonCostum color = 'silver' text='SigUp' />
-      
+  }}>
+    <TextInputCostum name = "name" color = "black"></TextInputCostum>
+    <TextInputCostum name = "Email" color ="red"></TextInputCostum> 
+    <TextInputCostum name = "password" color = "black"></TextInputCostum>
     </View>
-
-  </View>
-
   )
 }
 export default App
-
-
